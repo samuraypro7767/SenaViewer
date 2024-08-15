2a6f5fc9aa60e772c175f1ad2a5782cdc93cc30f
samuraypro7767 <sebaspro7767@gmail.com>
2024-08-15 10:27:36 -0500

    se crearon los getters y setters de la clase book

--- src/edu/misena/senaviewer/model/Chapter.java
+++ src/edu/misena/senaviewer/model/Chapter.java
@@ -1,4 +1,16 @@
 package edu.misena.senaviewer.model;
 
 public class Chapter {
+
+    int id;
+    String title;
+    int duration;
+    int year;
+    boolean viewed;
+    int timeViewed;
+    int sessionQuantity;
+
+    public Chapter( String title, int duration, int year){
+
+    }
 }
--- src/edu/misena/senaviewer/model/book.java
+++ src/edu/misena/senaviewer/model/book.java
@@ -1,5 +1,9 @@
 package edu.misena.senaviewer.model;
 
+import java.sql.Array;
+import java.util.ArrayList;
+import java.util.List;
+
 public class book {
 
     int id;
@@ -7,7 +11,7 @@ public class book {
 
     String edititionDate;
     String editorial;
-    String[] authors = new String[5];
+    String[] authors ;
     int isbn;
     boolean readed;
     int timeReaded;
@@ -23,17 +27,75 @@ public class book {
         this.isbn = isbn;
 
     }
-
-    public static  void see(){
-
+//  getter  método de acceso
+    public  int getId() {
+        return  id;
     }
 
-    public  static void getters(){
-
+//   setter método de modificación
+    public void setId(int id) {
+        this.id = id;
     }
 
-    public static void setters(){
 
+    public String getTitle() {
+        return  title;
+    }
+    public  void setTitle(String title) {
+        this.title = title;
+    }
+
+
+    public String getEdititionDate() {
+        return  edititionDate;
+    }
+    public  void setEdititionDate(String edititionDate) {
+        this.edititionDate = edititionDate;
+    }
+
+
+    public String getEditorial() {
+        return  editorial;
+    }
+    public  void setEditorial(String editorial) {
+        this.editorial = editorial;
+    }
+
+
+    public String[] getAuthors() {
+        return  authors;
+    }
+
+    public void setAuthors(String[] authors) {
+        if (authors == null) {
+            this.authors = new String[0];
+        } else {
+            this.authors = authors;
+        }
+    }
+
+
+    public int getIsbn() {
+        return  isbn;
+    }
+    public  void setIsbn(int isbn) {
+        this.isbn = isbn;
+    }
+
+
+    public boolean isReaded() {
+        return readed;
+    }
+    public void setReaded(boolean readed) {
+        this.readed = readed;
+    }
+
+
+    public int getTimeReaded() {
+        return timeReaded;
+    }
+    public void setTimeReaded(int timeReaded) {
+        this.timeReaded = timeReaded;
     }
 
 }
