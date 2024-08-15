859cf73aee8b5d52171c64dae79534f81641638e
samuraypro7767 <sebaspro7767@gmail.com>
2024-08-15 10:40:16 -0500

    empezando con los getters y setters de la clase chapter

--- src/edu/misena/senaviewer/model/Chapter.java
+++ src/edu/misena/senaviewer/model/Chapter.java
@@ -11,6 +11,33 @@ public class Chapter {
     int sessionQuantity;
 
     public Chapter( String title, int duration, int year){
+        this.title = title;
+        this.duration = duration;
+        this.year = year;
 
     }
+
+    //  getter  método de acceso
+    public  int getId() {
+        return  id;
+    }
+    //   setter método de modificación
+    public void setId(int id) {
+        this.id = id;
+    }
+
+
+    public String getTitle() {
+        return  title;
+    }
+    public  void setTitle(String title) {
+        this.title = title;
+    }
+
+
+
+
+
+
+
 }
