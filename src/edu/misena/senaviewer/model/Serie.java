a338c192f72b08b3f1ce7f76277151b8f32c0eb4
samuraypro7767 <sebaspro7767@gmail.com>
2024-08-22 08:18:42 -0500

    getter y setter de serie echos

--- src/edu/misena/senaviewer/model/Serie.java
+++ src/edu/misena/senaviewer/model/Serie.java
@@ -4,15 +4,91 @@ public class Serie {
 
     int id;
     String title;
-    String genre;
+
+    String gender;
     String creator;
     int duration;
-    int year;
+    short year;
+
     boolean viewed;
     int timeViewed;
     int sessionQuantity;
 
-    public Serie( String title, String genre, int duration){
+    public Serie( String title,String gender,int duration){
+        this.title = title;
+        this.gender = gender;
+        this.duration = duration;
+    }
 
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getGender() {
+        return gender;
+    }
+
+    public void setGender(String gender) {
+        this.gender = gender;
+    }
+
+    public String getCreator() {
+        return creator;
+    }
+
+    public void setCreator(String creator) {
+        this.creator = creator;
+    }
+
+    public int getDuration() {
+        return duration;
+    }
+
+    public void setDuration(int duration) {
+        this.duration = duration;
+    }
+
+    public short getYear() {
+        return year;
+    }
+
+    public void setYear(short year) {
+        this.year = year;
+    }
+
+    public boolean getViewed() {
+        return viewed;
+    }
+
+    public void setViewed(boolean viewed) {
+        this.viewed = viewed;
+    }
+
+    public int getTimeViewed() {
+        return timeViewed;
+    }
+
+    public void setTimeViewed(int timeViewed) {
+        this.timeViewed = timeViewed;
+    }
+
+    public int getSessionQuantity() {
+        return sessionQuantity;
+    }
+
+    public void setSessionQuantity(int sessionQuantity) {
+        this.sessionQuantity = sessionQuantity;
     }
 }
